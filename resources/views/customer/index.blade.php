caaf784db6e8b30db540303c9d09e29aac256b78
Robert Turner <robert.turner0106@gamil.com>
2021-11-15 18:33:48 +0530

    minor changes

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master')
 
-@section('title','User')
+@section('title','Customer')
 
 @section('content')
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
   <div class="toolbar" id="kt_toolbar">
     <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
       <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title me-3 mb-5 mb-lg-0">
-        <h1 class="text-dark fw-bolder fs-3 my-1 mt-5">User</h1>
+        <h1 class="text-dark fw-bolder fs-3 my-1 mt-5">Customer</h1>
       </div>
 
       <!--begin::Actions-->
