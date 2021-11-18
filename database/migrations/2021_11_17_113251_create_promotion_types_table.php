f6f005df50729d87e72282ca3633123e801d3746
Kayden R <kayden.r@manektech.dev>
2021-11-18 11:36:58 +0530

    promotion module related changes

---
+++ database/migrations/2021_11_17_113251_create_promotion_types_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotion_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        // Insert some stuff
+        DB::table('promotion_types')->insert(
+            array(
+                array('name' => 'Price Off Promo - single delivery'),
+                array('name' => 'Price Off Promo - 3 deliveries'),
+                array('name' => 'Price Off Promo - 5 deliveries'),
+                array('name' => 'Price Off Promo - 8 deliveries'),
+                array('name' => 'Bundle Discount Promo - 1 Delivery  per Product'),
+                array('name' => 'Bundle Discount Promo - 3 Deliveries  per Product'),
+                array('name' => 'Bundle Discount Promo - 5 Deliveries  per Product'),
+                array('name' => 'Bundle Discount Promo - 8 Deliveries  per Product'),
+                array('name' => 'Premium Reward Promo - 1 Delivery  per Product'),
+                array('name' => 'Premium Reward Promo - 3 Deliveries  per Product'),
+                array('name' => 'Premium Reward Promo - 5 Deliveries  per Product'),
+                array('name' => 'Premium Reward Promo - 38Deliveries  per Product')
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotion_types');
+    }
+}
