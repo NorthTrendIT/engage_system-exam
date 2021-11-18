cd631cdb35eaea078521f2b362c93da45d77a1f0
Robert Turner <robert.turner0106@gamil.com>
2021-11-18 11:59:51 +0530

    cron issues fix for customer and product

---
+++ app/Jobs/StoreProducts.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Product;
+
+class StoreProducts implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if(!empty($this->data)){
+
+            foreach ($this->data as $value) {
+                    
+                $insert = array(
+
+                                'item_code' => @$value['ItemCode'],
+                                'item_name' => @$value['ItemName'],
+                                'foreign_name' => @$value['ForeignName'],
+                                'items_group_code' => @$value['ItemsGroupCode'],
+                                'customs_group_code' => @$value['CustomsGroupCode'],
+                                'sales_vat_group' => @$value['SalesVATGroup'],
+                                'purchase_vat_group' => @$value['PurchaseVATGroup'],
+                                'created_date' => @$value['CreateDate']." ".@$value['CreateTime'],
+                                'is_active' => @$value['Valid'] == "tYES" ? true : false,
+                                //'response' => json_encode($value),
+                            );
+
+                $obj = Product::updateOrCreate(
+                                        [
+                                            'item_code' => @$value['ItemCode'],
+                                        ],
+                                        $insert
+                                    );
+            }
+
+        }
+    }
+}
