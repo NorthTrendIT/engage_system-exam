2141e86e86af92c098a57dc158085efe42d18a2f
unknown <imujaspatel@gmail.com>
2021-12-16 20:00:34 +0530

    role module access middleware changes

--- app/Http/Middleware/CheckRoleModuleAccess.php
+++ app/Http/Middleware/CheckRoleModuleAccess.php
@@ -37,7 +37,7 @@ class CheckRoleModuleAccess
                 // User Module
                 if(in_array($request->route()->getName(), ['user.index','user.get-all','user.show'])){
 
-                    if($access['view-user'] != 1){
+                    if(@$access['view-user'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for user module.";
@@ -46,7 +46,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['user.create'])){
 
-                    if($access['add-user'] != 1){
+                    if(@$access['add-user'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for create user.";
@@ -56,14 +56,14 @@ class CheckRoleModuleAccess
                 }elseif(in_array($request->route()->getName(), ['user.store'])){
 
                     if(isset($request->id)){
-                        if($access['edit-user'] != 1){
+                        if(@$access['edit-user'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for edit user.";
 
                         }
                     }else{
-                        if($access['add-user'] != 1){
+                        if(@$access['add-user'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for create user.";
@@ -73,7 +73,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['user.edit','user.status'])){
 
-                    if($access['edit-user'] != 1){
+                    if(@$access['edit-user'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for edit user.";
@@ -81,7 +81,7 @@ class CheckRoleModuleAccess
                     }
 
                 }elseif(in_array($request->route()->getName(), ['user.destroy'])){
-                    if($access['delete-user'] != 1){
+                    if(@$access['delete-user'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for delete user.";
@@ -92,14 +92,14 @@ class CheckRoleModuleAccess
                 // Customer Module
                 if(in_array($request->route()->getName(), ['customer.index','customer.get-all'])){
 
-                    if($access['view-customer'] != 1){
+                    if(@$access['view-customer'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for customer module.";
 
                     }
                 }elseif(in_array($request->route()->getName(), ['customer.sync-customers'])){
-                    if($access['add-customer'] != 1){
+                    if(@$access['add-customer'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sync customers.";
@@ -110,14 +110,14 @@ class CheckRoleModuleAccess
                 // Sales Person Module
                 if(in_array($request->route()->getName(), ['sales-persons.index','sales-persons.get-all'])){
 
-                    if($access['view-sales-person'] != 1){
+                    if(@$access['view-sales-person'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sales person module.";
 
                     }
                 }elseif(in_array($request->route()->getName(), ['sales-persons.sync-sales-persons'])){
-                    if($access['add-sales-person'] != 1){
+                    if(@$access['add-sales-person'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sync sales persons.";
@@ -129,14 +129,14 @@ class CheckRoleModuleAccess
                 // Product Module
                 if(in_array($request->route()->getName(), ['product.index','product.get-all','product.show'])){
 
-                    if($access['view-product'] != 1){
+                    if(@$access['view-product'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for product module.";
 
                     }
                 }elseif(in_array($request->route()->getName(), ['product.sync-products'])){
-                    if($access['add-product'] != 1){
+                    if(@$access['add-product'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sync products.";
@@ -145,14 +145,14 @@ class CheckRoleModuleAccess
                 }elseif(in_array($request->route()->getName(), ['product.store'])){
 
                     if(isset($request->id)){
-                        if($access['edit-product'] != 1){
+                        if(@$access['edit-product'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for edit product.";
 
                         }
                     }else{
-                        if($access['add-product'] != 1){
+                        if(@$access['add-product'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for create product.";
@@ -161,7 +161,7 @@ class CheckRoleModuleAccess
                     }
                 }elseif(in_array($request->route()->getName(), ['product.edit'])){
 
-                    if($access['edit-product'] != 1){
+                    if(@$access['edit-product'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for edit product.";
@@ -171,14 +171,14 @@ class CheckRoleModuleAccess
 
                 // Invoice Module
                 if(in_array($request->route()->getName(), ['invoices.index','invoices.get-all'])){
-                    if($access['view-invoice'] != 1){
+                    if(@$access['view-invoice'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for invoice module.";
 
                     }
                 }elseif(in_array($request->route()->getName(), ['invoices.sync-invoices'])){
-                    if($access['add-invoice'] != 1){
+                    if(@$access['add-invoice'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sync invoices.";
@@ -189,7 +189,7 @@ class CheckRoleModuleAccess
                 // Order Module
                 if(in_array($request->route()->getName(), ['orders.index','orders.get-all'])){
 
-                    if($access['view-order'] != 1){
+                    if(@$access['view-order'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for order module.";
@@ -207,7 +207,7 @@ class CheckRoleModuleAccess
                 // Location Module
                 if(in_array($request->route()->getName(), ['location.index','location.get-all'])){
 
-                    if($access['view-location'] != 1){
+                    if(@$access['view-location'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for location module.";
@@ -216,7 +216,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['location.create'])){
 
-                    if($access['add-location'] != 1){
+                    if(@$access['add-location'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for create location.";
@@ -226,14 +226,14 @@ class CheckRoleModuleAccess
                 }elseif(in_array($request->route()->getName(), ['location.store'])){
 
                     if(isset($request->id)){
-                        if($access['edit-location'] != 1){
+                        if(@$access['edit-location'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for edit location.";
 
                         }
                     }else{
-                        if($access['add-location'] != 1){
+                        if(@$access['add-location'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for create location.";
@@ -243,7 +243,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['location.edit','location.status'])){
 
-                    if($access['edit-location'] != 1){
+                    if(@$access['edit-location'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for edit location.";
@@ -251,7 +251,7 @@ class CheckRoleModuleAccess
                     }
 
                 }elseif(in_array($request->route()->getName(), ['location.destroy'])){
-                    if($access['delete-location'] != 1){
+                    if(@$access['delete-location'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for delete location.";
@@ -262,7 +262,7 @@ class CheckRoleModuleAccess
                 // Department Module
                 if(in_array($request->route()->getName(), ['department.index','department.get-all'])){
 
-                    if($access['view-department'] != 1){
+                    if(@$access['view-department'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for department module.";
@@ -271,7 +271,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['department.create'])){
 
-                    if($access['add-department'] != 1){
+                    if(@$access['add-department'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for create department.";
@@ -281,14 +281,14 @@ class CheckRoleModuleAccess
                 }elseif(in_array($request->route()->getName(), ['department.store'])){
 
                     if(isset($request->id)){
-                        if($access['edit-department'] != 1){
+                        if(@$access['edit-department'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for edit department.";
 
                         }
                     }else{
-                        if($access['add-department'] != 1){
+                        if(@$access['add-department'] != 1){
 
                             $status = false;
                             $message = "Oops ! you have not access for create department.";
@@ -298,7 +298,7 @@ class CheckRoleModuleAccess
 
                 }elseif(in_array($request->route()->getName(), ['department.edit','department.status'])){
 
-                    if($access['edit-department'] != 1){
+                    if(@$access['edit-department'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for edit department.";
@@ -306,7 +306,7 @@ class CheckRoleModuleAccess
                     }
 
                 }elseif(in_array($request->route()->getName(), ['department.destroy'])){
-                    if($access['delete-department'] != 1){
+                    if(@$access['delete-department'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for delete department.";
@@ -317,7 +317,7 @@ class CheckRoleModuleAccess
                 // Product List Module
                 // if(in_array($request->route()->getName(), ['product-list.index','product-list.show'])){
 
-                //     if($access['view-product-list'] != 1){
+                //     if(@$access['view-product-list'] != 1){
 
                 //         $status = false;
                 //         $message = "Oops ! you have not access for product list module.";
@@ -329,14 +329,14 @@ class CheckRoleModuleAccess
                 // Customer Module
                 if(in_array($request->route()->getName(), ['customer-group.index','customer-group.get-all'])){
 
-                    if($access['view-customer-group'] != 1){
+                    if(@$access['view-customer-group'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for customer group module.";
 
                     }
                 }elseif(in_array($request->route()->getName(), ['customer-group.sync-customer-groups'])){
-                    if($access['add-customer-group'] != 1){
+                    if(@$access['add-customer-group'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for sync customer groups.";
@@ -348,7 +348,7 @@ class CheckRoleModuleAccess
                 // Class Module
                 if(in_array($request->route()->getName(), ['class.index','class.get-all'])){
 
-                    if($access['view-class'] != 1){
+                    if(@$access['view-class'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for class module.";
@@ -359,7 +359,7 @@ class CheckRoleModuleAccess
                 // Product List Module
                 if(in_array($request->route()->getName(), ['product-list.index','product-list.get-all','product-list.show'])){
 
-                    if($access['view-product-list'] != 1){
+                    if(@$access['view-product-list'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for product list module.";
@@ -370,7 +370,7 @@ class CheckRoleModuleAccess
                 // My Promotions Module
                 if(in_array($request->route()->getName(), ['customer-promotion.index','customer-promotion.get-all','customer-promotion.show','customer-promotion.get-all-product-list','customer-promotion.store-interest','customer-promotion.product-detail','customer-promotion.order.index','customer-promotion.order.create','customer-promotion.order.show','customer-promotion.order.store','customer-promotion.order.get-all'])){
 
-                    if($access['view-my-promotions'] != 1){
+                    if(@$access['view-my-promotions'] != 1){
 
                         $status = false;
                         $message = "Oops ! you have not access for my promotions module.";
