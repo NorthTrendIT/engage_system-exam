f6ebd112d9381137854ca2b24bd219b5322f0bde
Ujas Patel <mt_0105@outlook.com>
2021-12-21 16:25:43 +0530

    sidebar changes

--- resources/views/layouts/include/sidebar.blade.php
+++ resources/views/layouts/include/sidebar.blade.php
@@ -51,7 +51,7 @@
             </div>
 
             @if(Auth::user()->role_id == 1 || ( (isset($access['view-customer-group']) && $access['view-customer-group'] == 1) || (isset($access['view-customer']) && $access['view-customer'] == 1) || (isset($access['view-class']) && $access['view-class'] == 1) ) )
-            <div data-kt-menu-trigger="click" class="menu-item menu-accordion {{ (in_array(request()->route()->getName(), ['customer.index','customer.show','customer-group.index','class.index','class.show', 'customers-sales-specialist.index', 'customers-sales-specialist.create', 'customers-sales-specialist.edit','customer-delivery-schedule.index', 'customer-delivery-schedule.create', 'customer-delivery-schedule.edit'])) ? 'hover show' : '' }}">
+            <div data-kt-menu-trigger="click" class="menu-item menu-accordion {{ (in_array(request()->route()->getName(), ['customer.index','customer.show','customer-group.index','class.index','class.show', 'customers-sales-specialist.index', 'customers-sales-specialist.create', 'customers-sales-specialist.edit','customer-delivery-schedule.index', 'customer-delivery-schedule.create', 'customer-delivery-schedule.edit', 'customer-delivery-schedule.show'])) ? 'hover show' : '' }}">
                <span class="menu-link">
                   <span class="menu-icon">
                      <!--begin::Svg Icon | path: icons/duotune/layouts/lay010.svg-->
@@ -103,7 +103,7 @@
 
                   @if(Auth::user()->role_id == 1)
                   <div class="menu-item">
-                     <a class="menu-link {{ (in_array(request()->route()->getName(), ['customer-delivery-schedule.index', 'customer-delivery-schedule.create', 'customer-delivery-schedule.edit'])) ? 'active' : '' }}" href="{{ route('customer-delivery-schedule.index') }}">
+                     <a class="menu-link {{ (in_array(request()->route()->getName(), ['customer-delivery-schedule.index', 'customer-delivery-schedule.create', 'customer-delivery-schedule.edit', 'customer-delivery-schedule.show'])) ? 'active' : '' }}" href="{{ route('customer-delivery-schedule.index') }}">
                         <span class="menu-bullet">
                         <span class="bullet bullet-dot"></span>
                         </span>
@@ -112,7 +112,7 @@
                   </div>
 
                   <div class="menu-item">
-                     <a class="menu-link {{ (in_array(request()->route()->getName(), ['customers-sales-specialist.index', 'customers-sales-specialist.create', 'customers-sales-specialist.edit'])) ? 'active' : '' }}" href="{{ route('customers-sales-specialist.index') }}">
+                     <a class="menu-link {{ (in_array(request()->route()->getName(), ['customers-sales-specialist.index', 'customers-sales-specialist.create', 'customers-sales-specialist.edit', 'customers-sales-specialist.show'])) ? 'active' : '' }}" href="{{ route('customers-sales-specialist.index') }}">
                         <span class="menu-bullet">
                         <span class="bullet bullet-dot"></span>
                         </span>
