bfeab29dc9c3f882454e846106c7119d06c2ab8c
Josh Turner <mt_0112@outlook.com>
2022-01-24 16:04:20 +0530

    WOrking on Product list issue.

--- resources/views/product-list/index.blade.php
+++ resources/views/product-list/index.blade.php
@@ -212,14 +212,12 @@ $(document).ready(function() {
               {data: 'item_name', name: 'item_name'},
             //   {data: 'brand', name: 'brand'},
             //   {data: 'item_code', name: 'item_code'},
-              @if(userrole() != 2)
-              {data: 'price', name: 'price'},
-              {data: 'action', name: 'action'},
-              @endif
               {data: 'brand', name: 'brand'},
               {data: 'u_item_line', name: 'u_item_line'},
               {data: 'u_tires', name: 'u_tires'},
+              @if(userrole() != 2)
               {data: 'price', name: 'price', orderable:false,searchable:false},
+              @endif
               {data: 'action', name: 'action', orderable:false,searchable:false},
           ],
           drawCallback:function(){
