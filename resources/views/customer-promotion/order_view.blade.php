a20ea426f83ad165af054314032bb158fd1cd8d3
Ujas Patel <mt_0105@outlook.com>
2022-03-07 14:54:29 +0530

    order status changes

--- resources/views/customer-promotion/order_view.blade.php
+++ resources/views/customer-promotion/order_view.blade.php
@@ -367,23 +367,25 @@
                                             
                                             <div class="row justify-content-between">
                                               <div class="order-tracking {{ in_array('Pending',getOrderStatusProcessArray($status)) ? "completed" : "" }}">
-                                                <span class="is-complete"></span>
+                                                <span class="is-complete"><img src="{{ asset('assets/assets/media/svg/order/pending.svg') }}"></span>
                                                 <p>Pending</p>
                                               </div>
                                               <div class="order-tracking {{ in_array('On Process',getOrderStatusProcessArray($status)) ? "completed" : "" }}">
-                                                <span class="is-complete"></span>
+                                                <span class="is-complete"><img src="{{ asset('assets/assets/media/svg/order/on-process.svg') }}"></span>
                                                 <p>On Process</p>
                                               </div>
                                               <div class="order-tracking {{ in_array('For Delivery',getOrderStatusProcessArray($status)) ? "completed" : "" }}">
-                                                <span class="is-complete"></span>
+                                                <span class="is-complete"><img src="{{ asset('assets/assets/media/svg/order/for-delivery.svg') }}"></span>
                                                 <p>For Delivery</p>
                                               </div>
                                               <div class="order-tracking {{ in_array('Delivered',getOrderStatusProcessArray($status)) ? "completed" : "" }}">
-                                                <span class="is-complete"></span>
+                                                <span class="is-complete"><img src="{{ asset('assets/assets/media/svg/order/delivered.svg') }}"></span>
                                                 <p>Delivered</p>
                                               </div>
                                               <div class="order-tracking {{ in_array('Completed',getOrderStatusProcessArray($status)) ? "completed" : "" }}">
-                                                <span class="is-complete"></span>
+                                                <span class="is-complete">
+                                                  <img src="{{ asset('assets/assets/media/svg/order/completed.svg') }}">
+                                                </span>
                                                 <p>Completed</p>
                                               </div>
                                             </div>
