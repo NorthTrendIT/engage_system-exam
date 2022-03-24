80aff4e2edb6dda169c34f7fa321b6848a601e53
Ujas Patel <mt_0105@outlook.com>
2022-03-24 12:04:58 +0530

    fixed sap script log issues

--- app/Support/SAPInvoices.php
+++ app/Support/SAPInvoices.php
@@ -62,7 +62,7 @@ class SAPInvoices
 
         } catch (\Exception $e) {
 
-            if($this->log_id){
+            if(!empty($this->log_id)){
                 add_sap_log([
                         'status' => "error",
                         'error_data' => $e->getMessage(),
@@ -113,7 +113,7 @@ class SAPInvoices
 
                     //$this->addInvoicesDataInDatabase($data['odata.nextLink']);
                 } else {
-                    if($this->log_id){
+                    if(!empty($this->log_id)){
                         add_sap_log([
                             'status' => "completed",
                         ], $this->log_id);
--- app/Support/SAPQuotations.php
+++ app/Support/SAPQuotations.php
@@ -62,7 +62,7 @@ class SAPQuotations
             }
 
         } catch (\Exception $e) {
-            if($this->log_id){
+            if(!empty($this->log_id)){
                 add_sap_log([
                         'status' => "error",
                         'error_data' => $e->getMessage(),
@@ -113,7 +113,7 @@ class SAPQuotations
 
                     //$this->addQuotationsDataInDatabase($data['odata.nextLink']);
                 } else {
-                    if($this->log_id){
+                    if(!empty($this->log_id)){
                         add_sap_log([
                             'status' => "completed",
                         ], $this->log_id);
