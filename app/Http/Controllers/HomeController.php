d3130b5f8f890c2aca7e0dccd7d984025a1e9e6a
Nimesh Patel <nimesh.patel@manektech.com>
2023-02-20 08:10:45 +0530

    customer not sync proper

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -327,7 +327,8 @@ class HomeController extends Controller
         ini_set('max_execution_time', 18000);
             
         $sap = new SAPCustomer('APBW', 'API' , 'AP@46amb', '1');
-        $sap->addSpecificCustomerData('C14013');        
+        $sap->addSpecificCustomerData('C14013');
+                
         return "complete"; 
     }
     
