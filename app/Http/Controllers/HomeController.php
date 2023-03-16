3a28d54294b4f311426bd33462b54db5045f83d4
Michael <itsupport@northtrend.com>
2023-03-16 14:31:54 +0800

    manual sync for customer C02524 NTMC

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -327,7 +327,7 @@ class HomeController extends Controller
         ini_set('max_execution_time', 18000);
             
         $sap = new SAPCustomer('NTMC', 'API' , 'AP@46amb', '1');
-        $sap->addSpecificCustomerData('C14071');
+        $sap->addSpecificCustomerData('C02524');
                 
         return "complete"; 
     }
