b02234dfbf793b94ad84744a63fb60384815fe3b
Michael <itsupport@northtrend.com>
2023-03-27 14:27:54 +0800

    add comment in database connection setup

--- app/Support/SAPCustomer.php
+++ app/Support/SAPCustomer.php
@@ -214,7 +214,7 @@ class SAPCustomer
                 $data[] = $response['data'];
                 if($data){
 
-                    StoreCustomers::dispatch($data, 5);
+                    StoreCustomers::dispatch($data, 5); //1.APBW, 2.NTMC, 3.PHILCREST, 4.PHILSYN [X], 5.SOLID TREND
                     if(isset($data['odata.nextLink'])){
                         //Log::info(print_r($this->database,true));
                         SyncNextCustomers::dispatch($this->database, $this->username, $this->password, $data['odata.nextLink'], $this->log_id, $this->search);
