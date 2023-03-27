d1f24f1ab9ea11f52cdf745a41da142cd3b0ea7c
Michael <itsupport@northtrend.com>
2023-03-27 14:21:15 +0800

    fix error in manual syncs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -326,7 +326,7 @@ class HomeController extends Controller
         ini_set('memory_limit', '10240M');
         ini_set('max_execution_time', 18000);
             
-        $sap = new SAPCustomer('APBW', 'API' , 'AP@46amb', '1');
+        $sap = new SAPCustomer('APBW', 'API' , 'AP@46amb', '1', '');
         $sap->addSpecificCustomerData('C14135');
                 
         return "complete"; 
