af59efee4f585e5ab64f63f10706457dff419b86
Michael <itsupport@northtrend.com>
2023-04-11 14:59:51 +0800

    added customer query for orders to be displayed per sales specialist

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -329,7 +329,7 @@ class OrdersController extends Controller
             $customers = Auth::user()->get_multi_customer_details();
             $data->whereIn('card_code', array_column($customers->toArray(), 'card_code'));
             $data->whereIn('sap_connection_id', array_column($customers->toArray(), 'sap_connection_id'));
-        }elseif(userrole() == 12){ //sales personnel
+        }elseif(userrole() == 14){ //sales personnel
             // $data->where('sales_person_code', @Auth::user()->sales_employee_code); //previous code
             $data->whereHas('customer', function($q){
                 $cus = CustomersSalesSpecialist::where(['ss_id' => Auth::user()->id])->pluck('customer_id')->toArray();
