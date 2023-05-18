0b9d43492bbf6a6fffac15296b6d133eee5f86ab
Michael <itsupport@northtrend.com>
2023-05-18 17:23:19 +0800

    sync

--- app/Support/SAPCustomer.php
+++ app/Support/SAPCustomer.php
@@ -107,9 +107,8 @@ class SAPCustomer
                 if($this->search){
                     $url = '/b1s/v1/BusinessPartners?$filter=contains(CardName, \''.$this->search.'\')';
                 }else{
-                    // $url = '/b1s/v1/BusinessPartners?$filter=VatGroup ne null and Valid eq \''.'tYES'.'\'';
-                    $url = '/b1s/v1/BusinessPartners?$filter=UpdateDate ge \''.$latestData->updated_date.'\' or CreateDate ge \''.$latestData->updated_date.'\'';
-                    //GroupCode eq 115 
+                    $url = '/b1s/v1/BusinessPartners?$filter=GroupCode eq 103 and VatGroup ne null and Valid eq \''.'tYES'.'\'';
+                    // $url = '/b1s/v1/BusinessPartners?$filter=UpdateDate ge \''.$latestData->updated_date.'\' or CreateDate ge \''.$latestData->updated_date.'\'';
                 }
                 $response = $this->getCustomerData($url);
                 // Log::info(print_r($response,true));
