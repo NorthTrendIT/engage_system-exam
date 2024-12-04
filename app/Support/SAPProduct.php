1b0cdc83b01ece7aa1e693e1f3e273539dfdd8c3
†Kingsman <itsupport@northtrend.com>
2024-12-04 16:13:00 +0800

    temporary fix for products date issue

--- app/Support/SAPProduct.php
+++ app/Support/SAPProduct.php
@@ -93,7 +93,7 @@ class SAPProduct
             if(!empty($latestData)){
                 $currentDate = Carbon::now(); 
                 $todaysDate = $currentDate->toDateString();
-                $previousDate = $currentDate->subDay()->toDateString(); // -1 day  //$date->subDays(3);
+                $previousDate = '2024-12-02'; //$currentDate->subDay()->toDateString(); // -1 day  //$date->subDays(3);
 
                 $url = '/b1s/v1/Items?$count=true&$filter=(UpdateDate ge \''.$previousDate.'\' and UpdateDate le \''.$todaysDate.'\') or (CreateDate ge \''.$previousDate.'\' and CreateDate le \''.$todaysDate.'\')';
                 $response = $this->getProductData($url);
