69f1eaa7cc313349753551847069e638e6c6dd5b
†Kingsman <itsupport@northtrend.com>
2025-04-22 16:57:05 +0800

    enhancing loading time when exporting sales order

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -1626,20 +1626,35 @@ class OrdersController extends Controller
             //     $query->orderBy('doc_time', 'desc');
             // });
         });
+
+        // $data->with([
+        //     'quotation' => function ($query) {
+        //         $query->select('id', 'doc_entry', 'sap_connection_id', 'u_omsno', 'doc_total');
+        //     },
+        //     'quotation.order' => function ($query) {
+        //         $query->select('u_omsno', 'sap_connection_id', 'doc_num');
+        //     },
+        //     'items' => function ($query) {
+        //         $query->select('id', 'local_order_id', 'total'); // Ensure local_order_id is included
+        //     },
+        // ]);
         
-        $data = $data->with([
-                            'customer.sap_connection',
-                            'customer.group',
-                            'customer',
-                            'quotation',
-                            'items',
-                            'address',
-                            'approver'
-        ]);
                         
         $records = [];
         $key_counter = 1;
         $data->chunk(1000, function ($orders) use (&$records, &$key_counter) {
+
+            $orders->load([
+                'quotation' => function ($query) {
+                    $query->select('id', 'doc_entry', 'sap_connection_id', 'u_omsno', 'doc_total');
+                },
+                'quotation.order' => function ($query) {
+                    $query->select('u_omsno', 'sap_connection_id', 'doc_num');
+                },
+                'items' => function ($query) {
+                    $query->select('id', 'local_order_id', 'total'); 
+                },
+            ]);
 
             $key_counter =+ $key_counter;
             foreach ($orders as $key => $value) {
